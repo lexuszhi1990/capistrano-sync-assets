bb7f748b12ce569a4870bc029312e1315c85f107
david <lexuszhi1990@gmail.com>
2015-06-10 15:06:24 +0800

    implement the asset for sync

--- lib/capistrano/sync/assets/asset.rb
+++ lib/capistrano/sync/assets/asset.rb
@@ -1,24 +1,27 @@
 module Capistrano
   module Sync
-    module Asset
+    class Asset
       def initialize(cap)
         @user_host = [cap.host.user, cap.host.hostname].compact.join('@')
         @port = cap.host.port || 22
         @shared_path = cap.shared_path
-        @sync_assets_dirs = cap.fetch(:sync_assets_dirs)
       end
 
 
-      def pull
+      def pull(dir)
         # system("rsync -a --del -L -K -vv --progress --rsh='ssh -p #{port}' #{user}@#{server}:#{cap.current_path}/#{dir} #{cap.fetch(:dir)}")
+        server_asset_dir = Utils.assets_dir "#{@shared_path}/#{dir}"
+        local_asset_dir = Utils.assets_dir "#{Dir.pwd}/#{dir}"
 
-        # "rsync -avrt --recursive --times --rsh=ssh --compress --human-readable --progress --delete user@server:/var/www/your-dir/public/ ~/your-dir/public/"
-
-        # info "Downloading config #{config} as #{local_path} "
+        puts "rsync -avrt --recursive --times --compress --human-readable --progress --delete --rsh='ssh -p #{@port}' #{@user_host}:#{server_asset_dir} #{local_asset_dir}"
       end
 
-      def push
+      def push(dir)
+        # system("rsync -a --del -L -K -vv --progress --rsh='ssh -p #{port}' #{user}@#{server}:#{cap.current_path}/#{dir} #{cap.fetch(:dir)}")
+        server_asset_dir = Utils.assets_dir "#{@shared_path}/#{dir}"
+        local_asset_dir = Utils.assets_dir "#{Dir.pwd}/#{dir}"
 
+        puts "rsync -avrt --recursive --times --compress --human-readable --progress --delete --rsh='ssh -p #{@port}' #{@user_host}:#{server_asset_dir} #{local_asset_dir}"
       end
     end
   end

b639d99a9d923a968271281cf56559d4eff1ff64
david <lexuszhi1990@gmail.com>
2015-06-10 09:58:38 +0800

    add initialize method to asset.rb

--- lib/capistrano/sync/assets/asset.rb
+++ lib/capistrano/sync/assets/asset.rb
@@ -1,8 +1,12 @@
-class Asset
-  def initialize(cap)
-    servers = Capistrano::Configuration.env.send(:servers)
-    server = servers.detect { |s| s.roles.include?(:app) }
-    port = server.netssh_options[:port] || 22
-    user = server.netssh_options[:user]
+module Capistrano
+  module Sync
+    module Assets
+      def initialize(cap)
+        servers = Capistrano::Configuration.env.send(:servers)
+        server = servers.detect { |s| s.roles.include?(:app) }
+        port = server.netssh_options[:port] || 22
+        user = server.netssh_options[:user]
+      end
+    end
   end
 end

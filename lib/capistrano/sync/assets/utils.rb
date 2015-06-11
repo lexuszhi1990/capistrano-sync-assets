aa1cd2de0128bf7a99a1cad73419032aaf01b5c1
david <lexuszhi1990@gmail.com>
2015-06-11 10:29:37 +0800

    add yes_or_no? method to utils

--- lib/capistrano/sync/assets/utils.rb
+++ lib/capistrano/sync/assets/utils.rb
@@ -1,7 +1,13 @@
 module Utils
   class << self
     def assets_dir(dir="")
-      dir.end_with? '/' ? dir : "#{dir}/"
+      # TODO: dir[0, dir.length-1] or dir.split('/').join("/")
+      dir.end_with? '/' ? dir.split('/').join("/") : dir
+    end
+
+    def yes_or_no?(msg, prompt = "(y)es, (n)o ")
+      ask(:answer, "#{msg} #{prompt} ? ")
+      (fetch(:answer) =~ /^y$|^yes$/i) == 0
     end
   end
 end

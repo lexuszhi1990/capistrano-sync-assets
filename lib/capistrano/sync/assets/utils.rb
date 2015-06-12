4d807e153d4b67b808e64a68b0ddf5cb076eacc8
david <lexuszhi1990@gmail.com>
2015-06-12 15:41:31 +0800

    move Utils to Capistrano::Sync modul

--- lib/capistrano/sync/assets/utils.rb
+++ lib/capistrano/sync/assets/utils.rb
@@ -1,28 +1,32 @@
-module Utils
-  class << self
-    def assets_dir(dir="")
-      # Benchmark.ips do |x|
-      #   x.report("dir[0, dir.length-1]") do
-      #     string = "/Users/david/bs_repos/tmp_repo/eio/"
-      #     string[0, string.length-1]
-      #   end
-      #
-      #   x.report("split('/').join") do
-      #     "/Users/david/bs_repos/tmp_repo/eio/".split('/').join("/")
-      #   end
-      #
-      #   x.compare!
-      # end
-      #
-      # Comparison:
-      # dir[0, dir.length-1]:  1844071.9 i/s
-      # split('/').join:        406966.0 i/s - 4.53x slower
-      dir.end_with?('/') ? dir[0, dir.length-1] : dir
-    end
+module Capistrano
+  module Sync
+    module Utils
+      class << self
+        def assets_dir(dir="")
+          # Benchmark.ips do |x|
+          #   x.report("dir[0, dir.length-1]") do
+          #     string = "/Users/david/bs_repos/tmp_repo/eio/"
+          #     string[0, string.length-1]
+          #   end
+          #
+          #   x.report("split('/').join") do
+          #     "/Users/david/bs_repos/tmp_repo/eio/".split('/').join("/")
+          #   end
+          #
+          #   x.compare!
+          # end
+          #
+          # Comparison:
+          # dir[0, dir.length-1]:  1844071.9 i/s
+          # split('/').join:        406966.0 i/s - 4.53x slower
+          dir.end_with?('/') ? dir[0, dir.length-1] : dir
+        end
 
-    def yes_or_no?(msg, prompt = "(y)es, (n)o")
-      ask(:answer, "#{msg} #{prompt}")
-      (fetch(:answer) =~ /^y$|^yes$/i) == 0
+        def yes_or_no?(msg, prompt = "(y)es, (n)o")
+          ask(:answer, "#{msg} #{prompt}")
+          (fetch(:answer) =~ /^y$|^yes$/i) == 0
+        end
+      end
     end
   end
 end
--- lib/capistrano/sync/tasks/assets.rake
+++ lib/capistrano/sync/tasks/assets.rake
@@ -11,9 +11,10 @@ namespace :sync do
           info "the assets dirs as #{sync_assets_dirs.join(', ')}"
           asset = Capistrano::Sync::Asset.new(self)
           sync_assets_dirs.each do |dir|
-            server_asset_dir = Utils.assets_dir "#{shared_path}/#{dir}"
-            local_asset_dir = Utils.assets_dir "#{Dir.pwd}/#{dir}"
-            if Utils.yes_or_no?("Are you SURE to upload the local assets to remote?")
+            # TODO: change the use of Capistrano::Sync::Utils to a light way
+            server_asset_dir = Capistrano::Sync::Utils.assets_dir "#{shared_path}/#{dir}"
+            local_asset_dir = Capistrano::Sync::Utils.assets_dir "#{Dir.pwd}/#{dir}"
+            if Capistrano::Sync::Utils.yes_or_no?("Are you SURE to upload the local assets to remote?")
               asset.backup_server_asset(dir)
               asset.push(local_asset_dir, server_asset_dir)
             end
@@ -31,8 +32,8 @@ namespace :sync do
             info "the assets dirs as #{sync_assets_dirs.join(', ')}"
             asset = Capistrano::Sync::Asset.new(self)
             sync_assets_dirs.each do |dir|
-              server_asset_dir = Utils.assets_dir "#{shared_path}/#{dir}"
-              local_asset_dir = Utils.assets_dir "#{Dir.pwd}/#{dir}"
+              server_asset_dir = Capistrano::Sync::Utils.assets_dir "#{shared_path}/#{dir}"
+              local_asset_dir = Capistrano::Sync::Utils.assets_dir "#{Dir.pwd}/#{dir}"
               info "backup: moving #{local_asset_dir} to #{local_asset_dir}_bak"
               asset.backup_local_asset(local_asset_dir)
               info "sync the #{local_asset_dir} from server #{server_asset_dir}"
